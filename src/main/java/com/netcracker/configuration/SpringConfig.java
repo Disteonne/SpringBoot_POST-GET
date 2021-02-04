97ef61e7eec49cf3e5c1063d3ecd3ba22294d40c
Disteonne <diana.razmikovna@yandex.ru>
2021-02-04 23:44:16 +0300

    v3

---
+++ src/main/java/com/netcracker/configuration/SpringConfig.java
@@ -0,0 +1,24 @@
+package com.netcracker.configuration;
+
+import org.springframework.boot.web.servlet.MultipartConfigFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.util.unit.DataSize;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.servlet.MultipartConfigElement;
+
+@Configuration
+@ComponentScan
+@EnableWebMvc
+public class SpringConfig {
+
+    @Bean
+    MultipartConfigElement multipartConfigElement() {
+        MultipartConfigFactory factory = new MultipartConfigFactory();
+        factory.setMaxFileSize(DataSize.parse("128KB"));
+        factory.setMaxRequestSize(DataSize.parse("128KB"));
+        return factory.createMultipartConfig();
+    }
+}
--- src/main/java/com/netcracker/controller/UserController.java
+++ src/main/java/com/netcracker/controller/UserController.java
@@ -10,10 +10,11 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.servlet.view.RedirectView;
 
-import java.io.File;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.*;
 import java.util.List;
 
 @Controller
@@ -26,30 +27,31 @@ public class UserController {
 
     @GetMapping("/user")
     public String read(Model model) {
-        model.addAttribute("newUser", new User());
-        return "new";
+            model.addAttribute("newUser", new User());
+            return "newUser";
     }
 
     @PostMapping("/user")
     public String write(@ModelAttribute User newUser, Model model) {
-        newUser = userService.addUser(newUser);
-        File file = new File("infoOfUsers");
-        try (FileWriter fileWriter = new FileWriter(file, true)) {
-            fileWriter.write(mapper.writeValueAsString(newUser) + "\n");
-        } catch (IOException ex) {
-            System.out.println("Error: controller_write");
-        }
-        model.addAttribute("addUser", newUser);
-        return "result";
+
+            newUser = userService.addUser(newUser);
+            File file = new File("infoOfUsers");
+            try (FileWriter fileWriter = new FileWriter(file, true)) {
+                fileWriter.write(mapper.writeValueAsString(newUser) + "\n");
+            } catch (IOException ex) {
+                System.out.println("Error: controller_write");
+            }
+            model.addAttribute("addUser", newUser);
+            return "resultAddNewUser";
     }
 
-    @GetMapping("user/search")
+    @GetMapping("/user/search")
     public String searchUser(Model model) {
         model.addAttribute("searchUser", new SearchUser());
         return "search";
     }
 
-    @PostMapping("user/search")
+    @PostMapping("/user/search")
     public String searchUserGet(@ModelAttribute SearchUser searchUser, Model model) {
         List<User> users = userService.find(searchUser);
         if (users.isEmpty()) {
@@ -58,12 +60,26 @@ public class UserController {
         model.addAttribute("users", users);
         return "searchIsDone";
     }
-    /*
-    @GetMapping("/hello")
-    ResponseEntity<String> hello() {
-        return new ResponseEntity<>("<H1> Hello World! </H1>", HttpStatus.BAD_REQUEST);
-    }
 
-     */
+    @GetMapping("/fileIsEmpty")
+    public String fileIsEmpty(){
+        return "fileIsEmpty";
+    }
+    @PostMapping("/user/upload")
+    public RedirectView uploadFile(@RequestParam("file") MultipartFile multipartFile){
+        if(!multipartFile.isEmpty()){
+            try(BufferedOutputStream bufferedOutputStream=new BufferedOutputStream(
+                    new FileOutputStream(new File("File_upload.txt")))) {
+                byte[] file=multipartFile.getBytes();
+                bufferedOutputStream.write(file);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  new RedirectView("/user");
+        }else {
+            return new RedirectView("/fileIsEmpty");
+        }
+
+    }
 
 }
